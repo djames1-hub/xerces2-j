b6eb9f9592480f04ada6e474084e038acea1ee52
Dylan James <djjames455@gmail.com>
2022-04-10 20:53:46 -0400

    Fix bug 8

--- src/org/apache/xerces/util/PrimeNumberSequenceGenerator.java
+++ src/org/apache/xerces/util/PrimeNumberSequenceGenerator.java
@@ -37,10 +37,10 @@ final class PrimeNumberSequenceGenerator {
       421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 
       521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 
       619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727};
+    private static Random r = null;
     
-    // TODO: Make Random object an instance variable since Random object is used once and then discarded which produces mediocre randomness
     static void generateSequence(int[] arrayToFill) {
-        Random r = new Random();
+        r = new Random();
         for (int i = 0; i < arrayToFill.length; ++i) {
             arrayToFill[i] = PRIMES[r.nextInt(PRIMES.length)];
         }  
